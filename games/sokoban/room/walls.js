19897d60c9261215556bbd2cc38808e41a65f14c
Isko <iskren.jgd@gmail.com>
2014-12-24 20:42:37 +0200

    collect wall neighbours data

--- games/sokoban/room/walls.js
+++ games/sokoban/room/walls.js
@@ -5,19 +5,41 @@ define(function(require) {
 
 	var Walls = function(bunchOfTiles) {
 		var walls = [];
+		var lastColumn = bunchOfTiles[0].length - 1;
+		var lastRow = bunchOfTiles.length - 1;
 		bunchOfTiles.forEach(function(tileRow, rowIndex) {
-			console.log("row:"+rowIndex);
+			// console.log("row:"+rowIndex);
 			walls.push([]);
 			tileRow.forEach(function(tile, colIndex) {
+				if (tile.isWall() ) {
 
-				if ( tile && tile.isWall() ) {
-					// console.log("col:"+colIndex+"+X");
-					walls[rowIndex][colIndex] ="X";
-				} else {
-					// console.log("col:"+colIndex+"+_");
-					walls[rowIndex][colIndex] = "_";
+					var above = false;
+					var below = false;
+					if ( rowIndex === 0 ) {
+						// above = false;
+						below = bunchOfTiles[rowIndex+1][colIndex].isWall();
+					} else if ( rowIndex === lastRow ) {
+						above = bunchOfTiles[rowIndex-1][colIndex].isWall();
+						// below = false;
+					} else {
+						above = bunchOfTiles[rowIndex-1][colIndex].isWall();
+						below = bunchOfTiles[rowIndex+1][colIndex].isWall();
+					}
+
+					var left = false;
+					var right = false;
+					if ( colIndex === 0 ) {
+						// left = false;
+						right = tileRow[colIndex+1].isWall();
+					} else if ( colIndex === lastColumn ) {
+						left = tileRow[colIndex-1].isWall();
+						// right = false;
+					} else {
+						left = tileRow[colIndex-1].isWall();
+						right = tileRow[colIndex+1].isWall();
+					}
+
 				}
-
 			});
 		});
 		console.warn(walls);
