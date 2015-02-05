cb743e830bbd8d01878b9952ad26a1ff87b09380
Iskren.Stanislavov <iskren.jgd@gmail.com>
2015-02-05 11:01:27 +0200

    improve OOP: auto resize
    viewport in stage

--- games/sokoban.js
+++ games/sokoban.js
@@ -11,7 +11,6 @@ define(function(require) {
 
 	var Sokoban = function(){
 		Stage.call(this, "game", {"showBG":true});
-		this.viewport = new Viewport(this.canvas);
 		this.init();
 	};
 	Sokoban.prototype = Object.create(Stage.prototype);
@@ -40,8 +39,8 @@ define(function(require) {
 		this.currentRoom = new Room( level );
 		this.addChild(this.currentRoom);
 
-		this.viewport.setFittable(this.currentRoom);
-		this.viewport.resize();
+		this.setAutoFit(this.currentRoom);
+		this.resize();
 
 		this.commandList.reset(this.currentRoom.records);
  		this.handlers.refresh(this.currentRoom);
@@ -58,63 +57,6 @@ define(function(require) {
  				},that, true);
  			};
  		}, 100);
-	};
-
-var Viewport = function( canvas ){
-	this.canvas = canvas;
-	this.fittable = null;
-};
-
-Viewport.prototype.setFittable = function(fittable) {
-	this.fittable = fittable;
-};
-
-	Viewport.prototype.attachEvents = function() {
-		this.canvas.oncontextmenu = function (e) {
-		    e.preventDefault();
-		};
-		var viewport = function() {
-			window.scrollTo(0,1);
-			this.resize();
-		}.bind(this)
-		document.onmousemove = document.ontouchmove = function(e) {
-			e.preventDefault();
-		};
-		document.removeEventListener("orientationchange");
-		document.addEventListener("orientationchange", viewport);
-		window.removeEventListener('resize');
-		window.addEventListener('resize', viewport, true);
-	};
-
-	Viewport.prototype.resize = function() {
-		if ( !this.fittable ){
-			return;
-		}
-
-		//we neeed the current; XXX: may store it n update it on orientation changes
-		//XXX: can store the scale factors for both orientations
-		//XXX: and change the chosen one on each orientation change
-		var screenW = window.innerWidth
-			|| document.documentElement.clientWidth
-			|| document.body.clientWidth;
-
-		var screenH = window.innerHeight
-			|| document.documentElement.clientHeight
-			|| document.body.clientHeight;
-
-		var scaleH = screenH / this.fittable.H;
-
-		//choose proper scale factor
-		var scale = (screenW >= scaleH * this.fittable.W) ?
-			scaleH : 
-			(screenW / this.fittable.W);
-		this.canvas.width  = this.fittable.W;
-		this.canvas.height = this.fittable.H;
-		if ( !this.canvas.width || !this.canvas.height ){
-			throw "zero dimensions";
-		}
-		$(this.canvas).css("width", Math.floor(this.fittable.W * scale).toString()+"px");
-		$(this.canvas).css("height", Math.floor(this.fittable.H * scale).toString()+"px");
 	};
 
 	return Sokoban;
--- libs/stage.js
+++ libs/stage.js
@@ -5,27 +5,78 @@ define(function(require) {
 		createjs.Stage.call(this, canvasId);
 
 		this.initSettings(settings);
+		this.fittable = null;
+		this.autoFitListeners()
 	};
 
-	$.extend(Stage.prototype, createjs.Stage.prototype);
+	Stage.prototype = Object.create( createjs.Stage.prototype );
 
-	$.extend(Stage.prototype, {
-		initSettings: function(settings) {
-			this.enableMouseOver(-1);
-			this.enableDOMEvents(false);// mouse events
+	Stage.prototype.initSettings = function(settings) {
+		this.enableMouseOver(-1);
+		this.enableDOMEvents(false);// mouse events
 
-			createjs.Ticker.useRAF = true;
-			createjs.Ticker.setFPS(25);
-			createjs.Ticker.addEventListener("tick", this);//update the stage
+		createjs.Ticker.useRAF = true;
+		createjs.Ticker.setFPS(25);
+		createjs.Ticker.addEventListener("tick", this);//update the stage
 
-			if ( settings && settings.showBG ) {
-				this.bg = new createjs.Shape();
-				this.bg.graphics.beginFill("black").drawRect(0, 0, 10000, 10000);
-				this.addChild(this.bg);
-			}
-		},
+		if ( settings && settings.showBG ) {
+			this.bg = new createjs.Shape();
+			this.bg.graphics.beginFill("black").drawRect(0, 0, 10000, 10000);
+			this.addChild(this.bg);
+		}
+	};
 
-	});
+	Stage.prototype.setAutoFit = function(fittable) {
+		this.fittable = fittable;
+	};
+
+	Stage.prototype.autoFitListeners = function() {
+		this.canvas.oncontextmenu = function (e) {
+		    e.preventDefault();
+		};
+		var viewport = function() {
+			window.scrollTo(0,1);
+			this.resize();
+		}.bind(this)
+		document.onmousemove = document.ontouchmove = function(e) {
+			e.preventDefault();
+		};
+		document.removeEventListener("orientationchange");
+		document.addEventListener("orientationchange", viewport);
+		window.removeEventListener('resize');
+		window.addEventListener('resize', viewport, true);
+	};
+
+	Stage.prototype.resize = function() {
+		if ( !this.fittable ){
+			return;
+		}
+
+		//we neeed the current; XXX: may store it n update it on orientation changes
+		//XXX: can store the scale factors for both orientations
+		//XXX: and change the chosen one on each orientation change
+		var screenW = window.innerWidth
+			|| document.documentElement.clientWidth
+			|| document.body.clientWidth;
+
+		var screenH = window.innerHeight
+			|| document.documentElement.clientHeight
+			|| document.body.clientHeight;
+
+		var scaleH = screenH / this.fittable.H;
+
+		//choose proper scale factor
+		var scale = (screenW >= scaleH * this.fittable.W) ?
+			scaleH : 
+			(screenW / this.fittable.W);
+		this.canvas.width  = this.fittable.W;
+		this.canvas.height = this.fittable.H;
+		if ( !this.canvas.width || !this.canvas.height ){
+			throw "zero dimensions";
+		}
+		$(this.canvas).css("width", Math.floor(this.fittable.W * scale).toString()+"px");
+		$(this.canvas).css("height", Math.floor(this.fittable.H * scale).toString()+"px");
+	};
 
 	return Stage; 
 });
